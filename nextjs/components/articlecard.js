924d9ebf257288f3740a8221eea5a8c47bdbb81e
BoonieHat <38331628+BoonieHat@users.noreply.github.com>
2020-05-26 15:53:02 -0400

    wip

--- nextjs/components/articlecard.js
+++ nextjs/components/articlecard.js
@@ -3,18 +3,16 @@ import main from '../components/main.module.css';
 import client from '../client'
 import imageUrlBuilder from '@sanity/image-url'
 function urlFor(source) { return imageUrlBuilder(client).image(source) }
-
+//${urlFor(props.image).width(300).url()}
 function ArticleCard(props) {
   return (
-    <div className={main.articleCard} style={{ backgroundImage: `url( ${urlFor(props.image).width(300).url()} )` }} alt="Article Headline Image">
+    <div className={main.articleCard} style={{ backgroundImage: `url(null)` }} alt="Article Headline Image">
       <Link href="/article/[slug]" as={`/article/${props.slug}`}>
         <a>
-          <div className={main.articleCardOverlay}>
-            <h4>{props.title}</h4>
-            <article>
-              <time>{new Date(props.updated).toDateString()}</time>
-            </article>
-          </div>
+          <article className={main.mtAuto}>
+            <time>{new Date(props.updated).toDateString()}</time>
+            <h2>{props.title}</h2>
+          </article>
         </a>
       </Link>{' '}
     </div>
