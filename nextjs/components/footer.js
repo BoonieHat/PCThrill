18007ded094c0715b969ae9e6282c02bdc2916a8
BoonieHat <38331628+BoonieHat@users.noreply.github.com>
2020-05-26 12:23:01 -0400

    wip

--- nextjs/components/footer.js
+++ nextjs/components/footer.js
@@ -1,24 +1,27 @@
 import Link from 'next/link'
-import footercss from '../components/footer.module.css';
 import main from '../components/main.module.css';
+import SvgLogo from '../components/logo';
+
+
 function footer(props) {
   return (
     <footer className={main.container}>
-      <div className={footercss.socialmedia}>
-        <a href="">Twitter</a>
-        <a href="">Instagram</a>
-        <a href="">Pinterest</a>
-        <a href="">Reddit</a>
-      </div>
-      <div className={footercss.policy}>
-        Google
-        <a href="https://policies.google.com/privacy">Privacy Policy</a>
-        and
-        <a href="https://policies.google.com/terms">Terms of Service</a>
-        apply.
-      </div>
-      <div className={footercss.copyright}>
-        © 2020 PC Thrill
+      <div className={main.footer}>
+        <div className={main.socialmedia}>
+          <a href="">Twitter</a>
+          <a href="">Instagram</a>
+          <a href="">Pinterest</a>
+          <a href="">Reddit</a>
+        </div>
+        <div className={main.policy}>
+          Google
+          <a href="https://policies.google.com/privacy">Privacy Policy</a>
+          and
+          <a href="https://policies.google.com/terms">Terms of Service</a>
+          apply.
+        </div>
+        <SvgLogo className={`${main.footerLogo}`} />
+        <div className={main.copyright}>© 2020 PC Thrill</div>
       </div>
     </footer>
   )
